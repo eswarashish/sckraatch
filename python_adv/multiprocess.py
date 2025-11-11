5a8f6e87298b1bbb976c745a5cd84751d715de8e
Eswar <ashish.kusumanchi@gmail.com>
2025-11-11 18:15:17 +0530

    multiprocessors

---
+++ python_adv/multiprocess.py
@@ -0,0 +1,50 @@
+import multiprocessing as mp
+# gil i.e global interpretation lock makes one interpretter to only excute one thread at same time
+# hence multiprocessing makes to unclock multiple interpretters with each single thread
+import time
+import math
+results_a = []
+
+results_b = []
+
+results_c = []
+
+def make_calc_1(numbers):
+    for number in numbers:
+        results_a.append(math.sqrt(number**3))
+
+def make_calc_2(numbers):
+    for number in numbers:
+        results_b.append(math.sqrt(number**4))
+
+def make_calc_3(numbers):
+    for number in numbers:
+        results_c.append(math.sqrt(number**5))
+
+# without mp
+
+# if __name__ =='__main__':
+#     number_list = list(range(5000000))
+
+#     start = time.time()
+#     make_calc_1(numbers=number_list)
+#     make_calc_2(numbers=number_list)
+#     make_calc_3(numbers=number_list)
+#     end = time.time()
+#     print(end-start)
+
+# with mp
+
+if __name__ =='__main__':
+    number_list = list(range(5000000))
+    p1 = mp.Process(target=make_calc_1,args=(number_list,))# args is a tuple so comma in thhe (arg,)
+    p2 = mp.Process(target=make_calc_2,args=(number_list,))# args is a tuple so comma in thhe (arg,)
+    p3 = mp.Process(target=make_calc_3,args=(number_list,))# args is a tuple so comma in thhe (arg,)
+    
+
+    start = time.time()
+    p1.start()
+    p2.start()
+    p3.start()
+    end = time.time()
+    print(end-start)
